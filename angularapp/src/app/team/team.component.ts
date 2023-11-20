6aa32e749a39c6dfaf16d7a7ef9e88d8fed6832e
Iamneo <techadmin@iamneo.ai>
2023-11-20 19:17:27 +0000

    Backup Commit

--- angularapp/src/app/team/team.component.ts
+++ angularapp/src/app/team/team.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AdminService } from '../services/admin.service';
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent implements OnInit {
+  teamdata:any[]=[]
+  constructor(private ms:AdminService) {
 
-  constructor() { }
-
+    this.ms.getTeams().subscribe(data=>{this.teamdata.push(...data)})
+    console.log(this.teamdata)
+   }
   ngOnInit(): void {
   }
 
