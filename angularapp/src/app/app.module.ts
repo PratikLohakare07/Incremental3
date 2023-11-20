a7a49ab67bd35f78a5074c486511186b9841ff0c
Iamneo <techadmin@iamneo.ai>
2023-11-20 12:10:10 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './player/player.component';
 import { TeamComponent } from './team/team.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { OrganizerComponent } from './organizer/organizer.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AdminComponent } from './admin/admin.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerComponent,
-    TeamComponent
+    TeamComponent,
+    LoginComponent,
+    NavbarComponent,
+    OrganizerComponent,
+    RegistrationComponent,
+    AdminComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
