4a3568a6f7e833b9bc62b9e4da4019e68fab4766
Iamneo <techadmin@iamneo.ai>
2023-11-20 12:54:22 +0000

    Backup Commit

--- dotnetapp/Controllers/PlayerController.cs
+++ dotnetapp/Controllers/PlayerController.cs
@@ -1,24 +1,110 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Diagnostics;
-// using System.Linq;
-// using System.Threading.Tasks;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.Extensions.Logging;
-// using dotnetapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dotnetapp.Models;
  
-// namespace dotnetapp.Controllers
-// {
-//     [ApiController]
-//     [Route("[controller]")]
-//     public class PlayerController : ControllerBase
-//     {
-//         private readonly ApplicationDbContext _context;
+namespace dotnetapp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayerController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
  
-//         public PlayerController(ApplicationDbContext context)
-//         {
-//             _context = context;
-//         }
+        public PlayerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+              
+ 
+        
+ 
+        // public IActionResult GetTeams()
+        // {
+        //     var data = _context.Players.ToList();
+        //     return Ok(data);
+        // }
+        [HttpGet]
+        [Route("ShowPlayers")]
+        public IActionResult GetPlayers()
+        {
+            var data = _context.Players.ToList();
+            return Ok(data);
+        }
+ 
+        [HttpPost]
+        [Route("AddPlayer")]
+        public IActionResult Post(Player movie)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Players.Add(movie);
+                    _context.SaveChanges();
+                }
+                catch(System.Exception ex)
+                {
+                    return BadRequest(ex.InnerException.Message);
+                }
+            }
+            return Created("Record Added", movie);
+        }
        
-//     }
-// }
+        [HttpPut]
+        [Route("EditPlayer/{id}")]
+        public IActionResult PutPlayer(int id ,Player players)
+        {
+            var data = _context.Players.Find(id);
+ 
+        try
+        {
+ 
+                    if(ModelState.IsValid)
+                    {
+                        // Player player = new Player{};
+ 
+                        Player player = _context.Players.Find(id);
+                        player.Age = players.Age;
+                        player.Name = players.Name;
+                        player.Category = players.Category;
+                        player.BiddingPrice = players.BiddingPrice;
+                        _context.SaveChanges();
+                    }
+                    return Ok();                
+        }
+        catch(System.Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+           
+        }
+ 
+        [HttpDelete]
+        [Route("DeletePlayer/{id}")]
+        public IActionResult DeletePlayer(int id)
+        {
+            try
+            {
+                // var detail = _context.Players.Where(d=>d.Id==id);
+                // if(detail.Count() != 0)
+                // {
+                //     throw new Exception("Cannot Delete Player");
+                // }
+                var data = _context.Players.Find(id);
+                _context.Players.Remove(data);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+       
+    }
+    }
--- dotnetapp/Models/ApplicationDbContext.cs
+++ dotnetapp/Models/ApplicationDbContext.cs
@@ -9,6 +9,7 @@ namespace dotnetapp.Models;
         public virtual DbSet<Player>Players {get;set;}
         public virtual DbSet<Team> Teams {get;set;}
         public virtual DbSet<User> Users {get;set;}
+        
  
         public ApplicationDbContext(){}
  
